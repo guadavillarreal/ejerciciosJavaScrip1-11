0f2c80fa317f7c37b5c1a9097fb266b745b21b12
guadavillarreal <guada.villarreal.gnv@gmail.com>
2023-10-21 01:36:18 -0300

    planteo del ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,16 @@
+/*9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+Ejemplo:    input: Hola mundo
+            Output: oauo
+*/
+
+//let frase = prompt("Ingrese una frase");
+//var x = "Mozilla";
+let x = prompt("Ingrese una frase")
+let empty = " ";
+
+document.write("Mozilla is " + x.length + " code units long"+"<br>");
+/* "Mozilla is 7 code units long" */
+
+document.write("The empty string has a length of " + empty.length);
+/* "The empty string has a length of 0" */
