5abf19f9e36e2acfb6463e1031bcb4bde371b44d
guadavillarreal <guada.villarreal.gnv@gmail.com>
2023-11-01 02:20:21 -0300

    resolucion del ejercicio 9

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -4,12 +4,36 @@ Ejemplo:    input: Hola mundo
             Output: oauo
 */
 
-//let frase = prompt("Ingrese una frase");
-//var x = "Mozilla";
-//let x = prompt("Ingrese una frase")
-//let empty = " ";
-/*document.write("Mozilla is " + x.length + " code units long"+"<br>");
-/* "Mozilla is 7 code units long" */
+let frase = prompt("Ingrese una frase");
+let cantidad = frase.length;
+let contador=0;
 
-/*document.write("The empty string has a length of " + empty.length);*/
-/* "The empty string has a length of 0" */
+console.log(frase); //me muestra la frase
+console.log(cantidad); //me dice la cantidad de caracteres
+
+console.log(frase.toLowerCase()); //muestra el CAMBIo de LAS MAYUSCULAS a minusculas, si es que hay
+frase = frase.toLowerCase(); //CAMBIA LAS MAYUSCULAS  a minusculas, si es que hay
+
+for (let index = 0; index <= cantidad; index++) {
+  if (
+    frase.charAt(index) === "a" ||
+    frase.charAt(index) === "á" ||
+    frase.charAt(index) === "e" ||
+    frase.charAt(index) === "é" ||
+    frase.charAt(index) === "i" ||
+    frase.charAt(index) === "í" ||
+    frase.charAt(index) === "o" ||
+    frase.charAt(index) === "ó" ||
+    frase.charAt(index) === "u" ||
+    frase.charAt(index) === "ú"
+  ){
+    console.log(frase.charAt(index));
+    document.write("<p>"+ frase.charAt(index)+"</p>");
+    }
+    else{
+        contador++;
+    }
+}
+if (contador>=cantidad) {
+    document.write("La frase ingresada no posee vocales");
+}
